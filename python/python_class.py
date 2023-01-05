829d9e7a390bd611e844b0c36c5c4d16db8ad014
Monika3002 <monika25io038@satiengg.in>
2023-01-05 22:08:40 +0530

    update

---
+++ python/python_class.py
@@ -0,0 +1,37 @@
+# print()function
+# print("hello")
+# ESCAPSE SEQUENCES
+# \': single quotes
+# \": double quotes
+# \\: single backslash
+# \n: new line
+# \t: tab
+# \b: backspace
+# \r : takes the cursor to the beginning of the line.
+# \f: form feed
+# print("My name is 'Monika Basene'and my branch is 'IoT' ")
+# print("My name is \"Monika Basene\" \b and \n my branch is \"IoT\" ")
+# print("My  name is Monika Basene and \r my branch is IoT ")
+# DATATYPES
+# a = 14
+# print(a)
+# type() function: to check the type of character
+a = 46
+name = 'aman'
+marks = 99.9
+print(type(a))
+print(type(name))
+print(type(marks))
+# output:
+# <class 'int'>
+# <class 'str'>
+# <class 'float'>
+# l = ('aman', 'renu', 'tarun')
+# print("the type of list ",type(l))
+# OPERATORS
+# **:power
+# \\:floor division
+a = 2**6
+print(a)
+c = 12//6
+print(c)
---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+
+# print("My name is 'Monika Basene'and my branch is 'IoT' ")
+# print("My name is \"Mon
