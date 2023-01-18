328e33f5962351193752d37cb55df57751415e1b
Monika3002 <monika25io038@satiengg.in>
2023-01-18 23:34:14 +0530

    update

--- python/python_2044/prime_number.py
+++ python/python_2044/prime_number.py
@@ -1,7 +1,8 @@
+# Taking a number from user
 n=int(input("Enter a number : "))
 if n>1:
     for i in range(2,n):
-
+# checking the conditions for prime number
      if( n % i ==0):
          print (n," is not a prime number.")
          break
